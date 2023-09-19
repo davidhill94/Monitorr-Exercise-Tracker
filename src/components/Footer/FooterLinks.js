885f88cea812a2d0120f2dc13b9f66e00d079e18
David <david94hill@gmail.com>
2023-09-19 10:32:17 +0100

    creating links

--- src/components/Footer/FooterLinks.js
+++ src/components/Footer/FooterLinks.js
@@ -1,30 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const FooterLinks = () => {
+
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+        });
+    };
+
     return (
         <div className="w-full flex flex-row justify-evenly py-4">
             <div className="p-2">
-                <h5 className="font-bold">Dashboard</h5>
-                <ul>
-                    <li>Route</li>
-                    <li>Route</li>
-                    <li>Route</li>
-                </ul>
-            </div>
-            <div className="p-2">
-                <h5 className="font-bold">Users</h5>
-                <ul>
-                    <li>Route</li>
-                    <li>Route</li>
-                    <li>Route</li>
-                </ul>
-            </div>
-            <div className="p-2">
                 <h5 className="font-bold">Monitorr</h5>
+                <ul onClick={goToTop}>
+                    <li>
+                        <Link onClick={goToTop} to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link onClick={goToTop} to="/statistics">Statistics</Link>
+                    </li>
+                    <li>
+                        <Link onClick={goToTop} to="/about">About Mrr</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="p-2">
+                <h5 className="font-bold">Database</h5>
                 <ul>
-                    <li>Route</li>
-                    <li>Route</li>
-                    <li>Route</li>
+                    <li>
+                        <Link onClick={goToTop} to="/user">Add Users</Link>
+                    </li>
+                    <li>
+                        <Link onClick={goToTop} to="/user">Add Activities</Link>
+                    </li>
+                    <li>
+                        <Link onClick={goToTop} to="/create">Add Exercise</Link>
+                    </li>
                 </ul>
             </div>
         </div>
